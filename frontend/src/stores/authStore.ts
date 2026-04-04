1351c862e3b8166aa5487e569ecc442a96c77ef3
JCamro <cameroelguerajosue@gmail.com>
2026-04-03 19:10:29 -0500

    fix:direccionamiento

--- frontend/src/stores/authStore.ts
+++ frontend/src/stores/authStore.ts
@@ -8,7 +8,7 @@ interface AuthState {
 }
 
 const apiLogin = async (username: string, password: string) => {
-  const response = await fetch('http://localhost:8000/api/auth/login/', {
+  const response = await fetch(import.meta.env.VITE_API_URL + '/api/auth/login/' || 'http://localhost:8000/api/auth/login/', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ username, password }),
